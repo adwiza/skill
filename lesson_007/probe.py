5b391cc87f5df7ad667dd04ca2bafd3ca796b199
Acid Wizard <adwiz@yandex.ru>
2020-07-03 09:12:45 +0300

    Beavis and Batthead go in to the house

--- lesson_007/probe.py
+++ lesson_007/probe.py
@@ -11,20 +11,21 @@ class Man:
         self.fullness = 50
         self.food = 50
         self.money = 0
+        self.house = None
 
     def __str__(self):
-        return f'Я {self.name}, сытость {self.fullness}, еды осталось {self.food}, денег осталось {self.money}'
+        return f'Я {self.name}, сытость {self.fullness}'
 
     def work(self):
         cprint('{} сходил на работу'.format(self.name), color='blue')
-        self.money += 50
+        self.house.money += 50
         self.fullness -= 10
 
     def eat(self):
-        if self.food >= 10:
+        if self.house.food >= 10:
             cprint('{} поел'.format(self.name), color='yellow')
             self.fullness += 10
-            self.food -= 10
+            self.house.food -= 10
         else:
             cprint('{} нет еды'.format(self.name), color='red')
 
@@ -34,8 +35,8 @@ class Man:
 
     def shopping(self):
         if self.money >= 50:
-            self.money -= 50
-            self.food += 50
+            self.house.money -= 50
+            self.house.food += 50
             cprint(f'{self.name} сходил в магазин за едой', color='magenta')
         else:
             cprint(f'{self.name} деньги кончились', color='red')
@@ -48,9 +49,9 @@ class Man:
 
         if self.fullness < 20:
             self.eat()
-        elif self.food < 10:
+        elif self.house.food < 10:
             self.shopping()
-        elif self.money < 50:
+        elif self.house.money < 50:
             self.work()
         elif dice == 1:
             self.work()
@@ -59,10 +60,29 @@ class Man:
         else:
             self.play_DOTA()
 
+    def go_in_to_house(self, house):
+        self.house = house
+        cprint(f'{self.name} заехал в дом!!!', color='cyan')
+
+class House:
+
+    def __init__(self):
+        self.food = 10
+        self.money = 50
+
+    def __str__(self):
+        return f'В доме еды осталось {self.food}, денег осталось {self.money}'
+
 
 beavis = Man(name='Бивис')
 batthead = Man(name='Батхед')
-for day in range(1, 365):
+
+my_sweet_home = House()
+
+beavis.go_in_to_house(house=my_sweet_home)
+batthead.go_in_to_house(house=my_sweet_home)
+
+for day in range(1, 21):
     print(f'============= день {day} ==============')
     beavis.act()
     batthead.act()
