6dfdb00c38ba35505e701e0e079c984459385b03
Acid Wizard <adwiz@yandex.ru>
2020-08-27 10:13:50 +0300

    Project template created

---
+++ lesson_009/project_template.py
@@ -0,0 +1,29 @@
+class BaseClass(object):
+    def template_method(self):
+        self.step_one()
+        self.step_two()
+        self.step_three()
+
+    def step_one(self):
+        raise NotImplementedError()
+
+    def step_two(self):
+        raise NotImplementedError()
+
+    def step_three(self):
+        raise NotImplementedError()
+
+
+class Worker(BaseClass):
+    def step_one(self):
+        print('Первый шаг алгоритма')
+
+    def step_two(self):
+        print('Второй шаг алгоритма')
+
+    def step_three(self):
+        print('Третий шаг алгоритма')
+
+
+example = Worker()
+example.template_method()
