632774a03a427633da22b3e12e1b824fe14a93e0
Acid Wizard <adwiz@yandex.ru>
2020-11-20 10:03:21 +0300

    all pillow added

--- lesson_013/python_snippets/postcard_maker.py
+++ lesson_013/python_snippets/postcard_maker.py
@@ -13,16 +13,25 @@ class PostCardmaker:
         else:
             self.font_path = font_path
 
-    def make(self):
-
+    def make(self, resize=False, out_path=None):
         im = Image.open(self.template)
+        if resize:
+            w, h = im.size
+            im = im.resize((w // 2, h // 2))
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype(self.font_path, size=26)
+        y = im.size[1] - 10 - (10 + font.size) * 2
+        message = f'Привет {self.name}!'
+        draw.text((10, y), message, font=font, fill='red')
+        y = im.size[1] - 20 - font.size
+        message = f'С праздником тебя!'
+        draw.text((10, y), message, font=font, fill='red')
+        # im.show()
+        out_path = out_path if out_path else 'postcard.jpg'
+        im.save(out_path)
+        print(f'Postcard saved as {out_path}')
 
-        w, h = im.size
-        resized_im = im.resize((w // 2, h // 2))
 
-        draw = ImageDraw.Draw(resized_im)
-        font = ImageFont.truetype(self.font_path, size=30)
-        y = resized_im.size[1] - 20 - font.size
-        draw.text((10, y), 'hello', font=font, fill='red')
-        resized_im.show()
-        # resized_im.save('probe.jpg')
+if __name__ == '__main__':
+    maker = PostCardmaker(name='Вася')
+    maker.make(resize=True)
