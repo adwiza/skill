470f9e023c82a0df07a455db837bb0ad1b6e6eb1
Acid Wizard <adwiz@yandex.ru>
2020-11-24 15:33:25 +0300

    vk echo bot ready

--- vk_bot.py
+++ vk_bot.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import random
 
-from token import token
+from _token import token
 import vk_api
+from vk_api import bot_longpoll
+import string
 
 group_id = 200472146
 
@@ -11,8 +14,30 @@ class Bot:
         self.group_id = group_id
         self.token = token
 
+        self.vk = vk_api.VkApi(token=token)
+        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
+
+        self.api = self.vk.get_api()
+
     def run(self):
-        pass
+        for event in self.long_poller.listen():
+            try:
+                print('Получено событие')
+                self.on_event(event)
+            except Exception as err:
+                print(err)
+
+    def on_event(self, event):
+        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+            out_msg = f'Ты сказал: \"{event.object.text.upper()}\", я тебя понял!!!'
+            print(out_msg)
+            self.api.messages.send(
+                message=out_msg,
+                random_id=random.randint(0, 2 ** 20),
+                peer_id=event.object.peer_id,
+            )
+        else:
+            print('Мы пока не умеем обрабатывать события этого типа', event.type)
 
 
 if __name__ == '__main__':
