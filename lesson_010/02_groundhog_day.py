28db4e0e891095645eaaa9fb8ca7a78e65e80db6
Acid Wizard <adwiz@yandex.ru>
2020-11-27 14:28:31 +0300

    vk_bot logging activated

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -17,12 +17,12 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 from random import randint
-import logging_
+import logging
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 carma_level = 0
 carma_counter = 0
-logging_.basicConfig(filename="log.txt", level=logging_.ERROR)
+logging.basicConfig(filename="log.txt", level=logging.ERROR)
 
 
 def one_day():
@@ -52,22 +52,22 @@ while True:
         if probability == 1:
             alert = randint(1, 6)
             if alert == 1:
-                logging_.error('IamGodError')
+                logging.error('IamGodError')
                 raise BaseException("IamGodError")
             elif alert == 2:
-                logging_.error('DrunkError')
+                logging.error('DrunkError')
                 raise BaseException("DrunkError")
             elif alert == 3:
-                logging_.error('CarCrashError')
+                logging.error('CarCrashError')
                 raise BaseException("CarCrashError")
             elif alert == 4:
-                logging_.error('GluttonyError')
+                logging.error('GluttonyError')
                 raise BaseException("GluttonyError")
             elif alert == 5:
-                logging_.error('DepressionError')
+                logging.error('DepressionError')
                 raise BaseException("DepressionError")
             elif alert == 6:
-                logging_.error('SuicideError')
+                logging.error('SuicideError')
                 raise BaseException("SuicideError")
 
     carma_level += one_day()
--- vk_bot.py
+++ vk_bot.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 import random
+import logging
 
 from _token import token
 import vk_api
-from vk_api import bot_longpoll
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 group_id = 200472146
+
+log = logging.getLogger('bot')
+
+
+def configure_logging():
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter(fmt='%(levelname)s %(message)s'))
+    stream_handler.setLevel(logging.INFO)
+    log.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler('vkbot.log')
+    file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%d-%m-%Y %H:%M'))
+    file_handler.setLevel(logging.DEBUG)
+    log.addHandler(file_handler)
+    log.setLevel(logging.DEBUG)
+
+
+
 
 
 class Bot:
@@ -14,32 +34,32 @@ class Bot:
         self.token = token
 
         self.vk = vk_api.VkApi(token=token)
-        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
+        self.long_poller = VkBotLongPoll(self.vk, self.group_id)
 
         self.api = self.vk.get_api()
 
     def run(self):
         for event in self.long_poller.listen():
             try:
-                # print('Получено событие')
                 self.on_event(event)
-            except Exception as err:
-                print(err)
+            except Exception:
+                log.exception('Ошибка в обработке события!!! ')
 
     def on_event(self, event):
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.MESSAGE_NEW:
             event.object.text = str(event.object.text)
             out_msg = f'Ты сказал: \"{event.object.text.upper()}\", я тебя понял!!!'
-            # print(out_msg)
+            log.debug('Отправляем сообщение назад')
             self.api.messages.send(
                 message=out_msg,
                 random_id=random.randint(0, 2 ** 20),
                 peer_id=event.object.peer_id,
             )
         else:
-            print('Мы пока не умеем обрабатывать события этого типа', event.type)
+            log.info('Мы пока не умеем обрабатывать события этого типа %s', event.type)
 
 
 if __name__ == '__main__':
+    configure_logging()
     bot = Bot(group_id, token)
     bot.run()
