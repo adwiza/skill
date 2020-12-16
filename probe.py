7773c208ebaacaca72c3f36e6a9f8a834b417b8d
Acid Wizard <adwiz@yandex.ru>
2020-12-16 20:17:45 +0300

    practice module completed, VK bot worked

--- probe.py
+++ probe.py
@@ -1,7 +1,36 @@
-import  logging.config
-
+import logging.config
+import numpy as np
 from primes_package.main import print_primes
 from log_settings import log_config
 
 logging.config.dictConfig(log_config)
 print_primes(30)
+
+A = [[4, 2],
+     [9, 0]]
+
+B = [[3, 1],
+     [-3, 4]]
+
+C = np.dot(B, A)
+
+print(C)
+
+D = [[2, 1],
+     [-3, 0],
+     [4, -1]]
+
+E = [[5, -1, 6],
+     [-3, 0, 7]]
+
+F = np.dot(D, E)
+
+print(F)
+
+G = [[1, 2, 3]]
+H = [[4],
+     [5],
+     [6]]
+I = np.dot(H, G)
+
+print(I)
--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import random
 import logging
+import handlers
 
 try:
     import settings
@@ -27,6 +28,14 @@ def configure_logging():
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    """Состояние пользователя внутри сценария."""
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
     Echo bot для vk.com.
@@ -40,11 +49,10 @@ class Bot:
         """
         self.group_id = group_id
         self.token = token
-
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
-
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """Запуск бота."""
@@ -54,24 +62,72 @@ class Bot:
             except Exception:
                 log.exception('Ошибка в обработке события!!! ')
 
-    def on_event(self, event: VkBotEventType):
+    def on_event(self, event):
         """
         Отправлляет сообщение назад, если это текст.
 
         :param event: VkBotMessageEvent object
         :return: None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            event.object.text = str(event.object.text)
-            out_msg = f'Ты сказал: \"{event.object.text.upper()}\", я тебя понял!!!'
-            log.debug('Отправляем сообщение назад')
-            self.api.messages.send(
-                message=event.object.text,
-                random_id=random.randint(0, 2 ** 20),
-                peer_id=event.object.peer_id,
-            )
-        else:
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.info('Мы пока не умеем обрабатывать события этого типа %s', event.type)
+            return
+            # event.object.text = str(event.object.text)
+            # out_msg = f'Ты сказал: \"{event.object.text.upper()}\", я тебя понял!!!'
+            # log.debug('Отправляем сообщение назад')
+        user_id = event.object.peer_id
+        text = event.object.text
+
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id, text=text)
+        else:
+            # search indent
+            for intent in settings.INTENTS:
+                if any(token in text for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+                else:
+                    text_to_send = settings.DEFAULT_ANSWER
+
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id,
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+        # continue scenario
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next step
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                # switch to next step
+                state.step_name = step['next_step']
+            else:
+                # finish scenario
+                self.user_states.pop(user_id)
+        else:
+            # retry current step
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
 
 if __name__ == '__main__':
